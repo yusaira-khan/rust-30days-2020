3cd8df86c012b70025b1036b964f523daa91e92a
Yusaira Khan <yusaira.khan@mail.mcgill.ca>
2020-12-07 20:17:23 -0500

    add init commit

---
+++ src/bin/day0.rs
@@ -0,0 +1,12 @@
+use std::io::{self, Read};
+
+fn main() -> io::Result<()> {
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+    let mut handle = stdin.lock();
+
+    handle.read_to_string(&mut buffer)?;
+    println!("Hello, World.");
+    println!("{}", buffer);
+    Ok(())
+}
